c46ae08425318566783ee7d1255cff64c8fa2ecb
DX X <2519983675@qq.com>
2021-03-28 11:13:35 +0800

    ..

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,42 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //数字按钮绑定
+    connect(ui->Zero,&QPushButton::clicked,[this](){
+        OnClicked(Num,"0");
+    });
+    connect(ui->One,&QPushButton::clicked,[this](){
+        OnClicked(Num,"1");
+    });
+    connect(ui->Two,&QPushButton::clicked,[this](){
+        OnClicked(Num,"2");
+    });
+    connect(ui->Three,&QPushButton::clicked,[this](){
+        OnClicked(Num,"3");
+    });
+    connect(ui->Four,&QPushButton::clicked,[this](){
+        OnClicked(Num,"4");
+    });
+    connect(ui->Five,&QPushButton::clicked,[this](){
+        OnClicked(Num,"5");
+    });
+    connect(ui->Six,&QPushButton::clicked,[this](){
+        OnClicked(Num,"6");
+    });
+    connect(ui->Seven,&QPushButton::clicked,[this](){
+        OnClicked(Num,"7");
+    });
+    connect(ui->Eight,&QPushButton::clicked,[this](){
+        OnClicked(Num,"8");
+    });
+    connect(ui->Nine,&QPushButton::clicked,[this](){
+        OnClicked(Num,"9");
+    });
+
+    //运算符号
+    connect(ui->Zero,&QPushButton::clicked,[this](){
+        OnClicked(Num,"0");
+    });
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include<QString>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+enum BtnType
+{
+    Num,
+    Op,
+    Dot,
+    Equal,
+    Clear,
+    Back
+};
 
 class MainWindow : public QMainWindow
 {
@@ -17,5 +27,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QString num1;
+    QString num2;
+    QString op;
+public slots:
+    void OnClicked(BtnType _type,QString _btn){}
 };
 #endif // MAINWINDOW_H
